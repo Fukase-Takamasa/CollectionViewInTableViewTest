0203f50a808728470fc4c59180f173f9fa5f5c44
深瀬 貴将 <fukase@arsaga.jp>
2020-03-06 20:14:48 +0900

    Initial Commit

---
+++ CollectionViewInTableViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewInTableViewTest
+//
+//  Created by 深瀬 貴将 on 2020/03/06.
+//  Copyright © 2020 深瀬 貴将. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
